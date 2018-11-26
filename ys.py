859ac048de24671ba87675cf5e2d16a363aa522a
Mustafa Ozan Alpay <frozsgy@gmail.com>
2018-11-26 17:46:56 +0300

    Update ys.py

--- ys.py
+++ ys.py
@@ -1,13 +1,25 @@
+import sys
+import os
+import platform
 import requests
 import time
 
-url = "https://www.yemeksepeti.com/cajun-corner-100-yil-ankara?status=closed"
-r = requests.get(url)
+def screech():
+    sys.stdout.write('\a')
+    if platform.system() == 'Linux':
+        os.system("aplay -q kaspersky.wav")
+    elif platform.system() == 'Windows':
+        os.system("start /min mplay32 /play /close karspersky.wav")
 
-while(1):
-    if((r.content).find("beklemek istemiyor") > -1):
+# Edit the following URL to match your own favourite order place
+url = "https://www.yemeksepeti.com/cajun-corner-100-yil-ankara?status=closed"
+
+while True:
+    r = requests.get(url)
+    if (r.content).find("beklemek istemiyor") > -1:
         print "meh still closed :/"
         time.sleep(1)
     else :
         print "IT'S OPEN NOW RUN RUN RUN"
+        screech()
         break
